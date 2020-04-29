c898778b9d69dab757e2028f399abc7b2b9176d4
HarshwardhanPatil1329 <60012353+HarshwardhanPatil1329@users.noreply.github.com>
2020-04-29 23:32:04 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,13 @@
-const url = `http://newsapi.org/v2/top-headlines?country=in&apiKey=396f24a36ea64c608de1d62b555ac1c4`;
+const url ='http://newsapi.org/v2/top-headlines?' +
+          'country=in&' +
+          'apiKey=396f24a36ea64c608de1d62b555ac1c4';
 
-jQuery(document).ready(async function news() {
-    await fetch(url)
+jQuery(document).ready(async function news(){
+         await fetch(url)
         .then((res) => res.json())
         .then((articles) => {
-            let output = "";
-            articles.articles.forEach((articles) => {
+            let output="";
+            articles.articles.forEach((articles)=>{
                 output += `
                     <li class="article">
                        <img class="article-img" src="${article.urlToImage}"/>
@@ -16,28 +18,28 @@ jQuery(document).ready(async function news() {
                     </li>
                 `;
             });
-            document.getElementById("news-articles").innerHTML = output;
+            document.getElementById("news-articles").innerHTML=output;
         })
         .catch((err) => console.log(err));
-});
+    });
 
-$(document).ready(function() {
-    $('#search').keypress(() => {
-        let searchField = $("#search").val();
-        const url2 = `https://newsapi.org/v2/everything?q=${searchField}&apiKey=396f24a36ea64c608de1d62b555ac1c4`;
-
-        if (searchField !== "") {
-            $.ajax({
-                url: url2,
-                method: "GET",
-                dataType: "json",
-
-                success: function(news) {
-                    let output2 = "";
-                    let articals = news.articles;
-
-                    for (var art in articals) {
-                        output2 += `
+$(document).ready(function(){ 
+    $('#search').keypress(()=>{
+      let searchField = $("#search").val();
+      const url2 = `https://newsapi.org/v2/everything?q=${searchField}&apiKey=396f24a36ea64c608de1d62b555ac1c4`;
+      
+      if(searchField !== ""){
+        $.ajax({
+          url: url2,
+          method: "GET",
+          dataType: "json",
+          
+            success: function(news){
+            let output2 = "";
+            let articals = news.articles;
+            
+            for(var art in articals){
+              output2 +=`
                 <li class="article">
                    <img class="article-img" src="${art.urlToImage}"/>
                    <h2 class="article-title">${art.title}</h2>
@@ -45,15 +47,15 @@ $(document).ready(function() {
                    <span class="article-author">${art.author}</span>
                    <a class="article-link" href="${art.url}" target="_blank"></a>
                 </li>`;
-                    }
-                    if (output2 !== "") {
-                        document.getElementById("news-articles").innerHTML = output2;
-                    } else {
-                        $("#news-articles").html("<h1 class="
-                            not - found ">No article was found based on the search.</h1>");
-                    }
-                }
-            });
-        }
+            }
+            if(output2 !== ""){
+              document.getElementById("news-articles").innerHTML=output2;  
+            }
+            else{
+                $("#news-articles").html("<h1 class="not-found">No article was found based on the search.</h1>");
+            } 
+          }   
+        }); 
+      }
     });
 });
