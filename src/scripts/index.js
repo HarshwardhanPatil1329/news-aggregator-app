9581072921872c21e63a72a6e1159ec8f2d3e3fa
HarshwardhanPatil1329 <60012353+HarshwardhanPatil1329@users.noreply.github.com>
2020-05-03 15:35:34 +0530

    Update index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,32 +1,26 @@
-const url = 'http://newsapi.org/v2/top-headlines?' +
-    'country=in&' +
-    'apiKey=396f24a36ea64c608de1d62b555ac1c4';
+async function News() {
+const res = await fetch(
+    `http://newsapi.org/v2/top-headlines?country=in&apiKey=5da46edada014d7088420f175f3c0944`
+  );
+  const data = await res.json();
 
-jQuery(document).ready(async function news() {
-    await fetch(url)
-        .then((res) => res.json())
-        .then((articles) => {
-            let output = "";
-            articles.articles.forEach((article) => {
-                console.log(articles);
-                output += `
-                    <li class="article">
-                      <img class="article-img" src="${article.urlToImage}">
-
-                      <h2 class="article-title">${article.title}</h2>
-
-                      <p class="article-description">${article.description}</p>
-
-                      <span class="article-author">${article.author}</span>
-                        
-                      <a class="article-link" href="${article.url}" target="_blank"></a>
+  if (data.totalResults > 0) {
+    var output = "";
+    data.articles.forEach((art) => {
+    output += `<li class="article">
+                        <img class="article-img" src="${art.urlToImage}">
+                        <h2 class="article-title">${art.title}</h2>
+                        <p class="article-description">${art.description}</p>
+                        <span class="article-author">${art.author}</span>
+                        <a class="article-link" href="${art.url}" target="_blank"></a>
                     </li>
                 `;
-            });
-            document.getElementById("news-articles").innerHTML = output;
-        })
-        .catch((err) => console.log(err));
-});
+    document.getElementById("news-articles").innerHTML = output;
+    });
+  } 
+}
+News();
+
 
 $(document).ready(function() {
     $('#search').keypress(() => {
@@ -63,7 +57,7 @@ $(document).ready(function() {
                     if (output2 !== "") {
                         document.getElementById("news-articles").innerHTML = output2;
                     } else {
-                        $("#news-articles").html("<p class="not-found">No article was found based on the search.</p>");
+                        $("#news-articles").html("<h1>No article was found based on the search.</h1>");
                     }
                 }
             });
